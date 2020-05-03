d623f348c3f0005459421f7c07ab9b1e074f0dda
Kunal-khanwalkar <kunalkhanwalkar@gmail.com>
2020-05-03 20:30:20 +0530

    Added tweets and updated code

--- twitter_data.py
+++ twitter_data.py
@@ -27,7 +27,8 @@ class Da_tweets:
 		self.twitter_api = twitter.Api(consumer_key = apikeys[0],
 								consumer_secret = apikeys[1],
 								access_token_key = apikeys[2],
-								access_token_secret = apikeys[3])
+								access_token_secret = apikeys[3],
+								tweet_mode = 'extended')
 
 	def test_api(self):
 		print(self.twitter_api.VerifyCredentials())
@@ -41,7 +42,7 @@ class Da_tweets:
 				corpus.append({"tweet_id":row[2],"label":row[1],"topic":row[0]})					
 
 		rate_limit = 180
-		sleep_time = 900/180
+		sleep_time = 900/180	
 		trainingDataset = []
 
 		for tweet in corpus:
@@ -65,11 +66,11 @@ class Da_tweets:
 
 
 	def build_testset_keyword(self,keyword):
-		try:			
-			tweets_fetched = self.twitter_api.GetSearch(raw_query = keyword ,count = 10)
+		try:						
+			tweets_fetched = self.twitter_api.GetSearch(raw_query = keyword)
 			print('fetched ' + str(len(tweets_fetched)) + ' tweets for ' + keyword)			
 			for status in tweets_fetched:
-				self.normal_tweets.append({'text':status.text, 'label':None})
+				self.normal_tweets.append({'text':status.full_text,'id':status.id ,'label':None})							
 			self._preprocess_tweets()
 		except:
 			print('Some unfortunate event happened RIP maybe internet issues or stuff')
@@ -87,27 +88,108 @@ class Da_tweets:
 
 
 	def print_tweets(self):
+		print()
 		for tweet in self.normal_tweets:
-			print(tweet['text'])
+			print(tweet['text'],tweet['id'])
+		print()
 
 	def print_processed_tweets(self):
 		for tweet in self.processed_tweets:
 			print(tweet)
 
+	def getTweetKeys(self):
+		stats = self.twitter_api.GetUserTimeline(screen_name='@Microsoft',count=1)		
+		tweet = stats[0].AsDict()
+		tweetkeys = []
+		for key in tweet.keys():
+			tweetkeys.append(key)
+		return tweetkeys
 
+
+
+	def GetUserTweets(self,usernm,company):
+		self.normal_tweets = []
+
+		stats = self.twitter_api.GetUserTimeline(screen_name='@'+usernm,count=200)		
+		tweets = [i.AsDict() for i in stats]		
+		for tweet in tweets:
+			if(company.lower() in tweet['full_text'].lower()):
+				self.normal_tweets.append({'text':tweet['full_text'],'id':tweet['id'],'label':None})
+
+		max_tweets = 3240
+		iterations = int(max_tweets/200)
+		for i in range(iterations):
+			stats = self.twitter_api.GetUserTimeline(screen_name=usernm,max_id=tweets[-1]['id'],count=200)
+			tweets = [i.AsDict() for i in stats]			
+			for tweet in tweets:
+				if(company.lower() in tweet['full_text'].lower()):
+					self.normal_tweets.append({'text':tweet['full_text'],'id':tweet['id'],'label':None})
+		
+		print('fetched ' + str(len(self.normal_tweets)) + ' tweets for ' + company + ' from @' + usernm)			
+
+
+		userdatafile = '.\\data\\twitter\\' + company + '_finance.csv'
+		with open(userdatafile,'a') as csvfile:
+			lineWriter = csv.writer(csvfile,delimiter=',',quotechar='\"')
+			for tweet in self.normal_tweets:
+				try:
+					lineWriter.writerow([tweet['text'],tweet['id'],tweet['label']])
+				except Exception as e:
+					print(e)
+
+		self._preprocess_tweets()
+
+
+	def socialTweets(self,company):
+		self.normal_tweets = []
+		query = 'q=to%3A' + company + '%20-filter%3Alinks' + '&count=100'
+
+		self.build_testset_keyword(query)
+
+		total_tweets = 2500
+		iterations = int(total_tweets/100)
+
+		for i in range(iterations):
+			last_tweet = self.normal_tweets[-1]	
+			query = 'q=to%3A' + company + '%20-filter%3Alinks' + '&max_id='+ str(last_tweet['id']) + '&count=100'
+			self.build_testset_keyword(query)
+
+
+		userdatafile = '.\\data\\twitter\\' + company + '_peepeepoopoo.csv'
+		with open(userdatafile,'w') as csvfile:
+			lineWriter = csv.writer(csvfile,delimiter=',',quotechar='\"')
+			for tweet in self.normal_tweets:
+				try:
+					lineWriter.writerow([tweet['text'],tweet['id'],tweet['label']])
+				except Exception as e:
+					print(e)
+
+		self._preprocess_tweets()
+
+		
 
 
 if __name__=='__main__':
 
-	companies = ['Apple','Amazon','Facebook','Google','Intel','Microsoft','Samsung','Walmart']
+	companies = ['Apple','Amazon','Facebook','GoldmanSachs','Google','Intel','jpmorgan','Microsoft','Tesla','Samsung','Walmart']
+	users = ['CNBC','Benzinga','Stocktwits','BreakoutStocks','bespokeinvest','WSJmarkets','Stephanie_Link','nytimesbusiness','IBDinvestors','WSJdeals']
 
 	api_file = '.\\APIkeys.txt'
-	corpusfile = '.\\data\\corpus.csv'
-	tweetfile = '.\\data\\tweetdata.csv'
-	twitter_time = Da_tweets(api_file)
-	#twitter_time.test_api()
-	#twitter_time.build_trainingset(corpusfile,tweetfile)	
 
+	twitter_time = Da_tweets(api_file)
+	
+	for company in companies:			
+		twitter_time.socialTweets(company)
+
+		for usernm in users:
+			twitter_time.GetUserTweets(usernm,company)
+		
+		print(len(twitter_time.normal_tweets))
+
+
+	#corpusfile = '.\\data\\corpus.csv'
+	#tweetfile = '.\\data\\tweetdata.csv'
+'''
 	for company in companies:	
 		query = 'q=' + company + '%20%23' + company + '%20%40' + company + '%20lang%3Aen%20-filter%3Alinks%20-filter%3Areplies&'		
 		twitter_time.build_testset_keyword(query)		
@@ -118,8 +200,8 @@ if __name__=='__main__':
 				try:
 					lineWriter.writerow([tweet["text"],tweet["label"]])
 				except Exception as e:
-					print(e)
-		
-	#twitter_time.build_testset_keyword('Microsoft')	
+						print(e)
+'''		
+	
 	#twitter_time.print_processed_tweets()
 	 
